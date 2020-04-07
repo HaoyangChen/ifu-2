b1c26b30e597bde7e9146f4241a5365873d00024
YtMa <ytma98@gmail.com>
2020-04-06 22:09:46 -0700

    Move original files to new repo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,51 +1,81 @@
 export default {
-  mode: 'universal',
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [],
-  /*
-   ** Build configuration
-   */
-  build: {
+    mode: 'universal',
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    extend(config, ctx) {}
-  }
-}
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || '',
+            },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#fff' },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [
+        // Doc: https://github.com/nuxt-community/eslint-module
+        '@nuxtjs/eslint-module',
+    ],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        [
+            'nuxt-i18n',
+            {
+                detectBrowserLanguage: {
+                    useCookie: true,
+                    fallbackLocale: 'zh',
+                },
+                langDir: 'lang/',
+                locales: [
+                    {
+                        code: 'zh',
+                        iso: 'zh-CN',
+                        name: 'Chinese',
+                        file: 'zh.js',
+                    },
+                    {
+                        code: 'en',
+                        iso: 'en-US',
+                        name: 'English',
+                        file: 'en.js',
+                    },
+                ],
+                strategy: 'no_prefix',
+                lazy: true,
+            },
+        ],
+    ],
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {},
+    },
+};
