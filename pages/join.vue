7f96b62ebce0f8ba72b0b5cd8cf8900998050b5f
YtMa <ytma98@gmail.com>
2020-06-28 13:34:48 -0700

    Join page scaffold

---
+++ pages/join.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <Banner
+            title="加入我们"
+            button-text="我们的故事"
+            to="/about"
+            description="如果你有一颗帮助留学群体热忱的心；如果你希望尽自己的一份力让留学家庭更安心；让父母和海外学子之间的信息代沟有所改善；让留学生在海外的心理焦虑有所减少；那么就来加入我们IFU大家庭吧！"
+            :background="bannerImage"
+        />
+        <Tab :list="tabList" />
+    </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue';
+import Tab from '@/components/Tab.vue';
+import Volunteer from '@/tabs/join/volunteer.vue';
+import Staff from '@/tabs/join/staff.vue';
+import bannerImage from '@/assets/join/banner.png';
+
+export default {
+    components: {
+        Banner,
+        Tab,
+    },
+    data() {
+        return {
+            bannerImage,
+        };
+    },
+    computed: {
+        tabList() {
+            return [
+                {
+                    title: '志愿者招募',
+                    content: Volunteer,
+                    color: '#C9D74A',
+                },
+                {
+                    title: '工作人员招募',
+                    content: Staff,
+                    color: '#F96291',
+                },
+            ];
+        },
+    },
+};
+</script>
---
+++ tabs/join/staff.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <div class="row-1">
+            <section>
+                <h2>加入工作人员团队</h2>
+                <p>
+                    IFU的工作人员负责组织的日常运营。组织分为运营、志愿者管理、行政、项目、技术、对外关系六大部门。工作人员直接申请要求有至少3个月以上的工作经验，需通过递交简历申请、面试、General
+                    Test和3个月的考核期，通过后将根据考核结果进入对应职位带领志愿者达成组织使命。
+                </p>
+            </section>
+            <div>
+                <img src="@/assets/join/staff/staff.png" />
+            </div>
+        </div>
+        <section class="join-section">
+            <p>
+                感谢您对我们的认可，IFU目前暂无工作人员岗位招聘。考虑作为志愿者加入我们？
+            </p>
+            <nuxt-link to="/join">查看志愿者招募职位>>></nuxt-link>
+        </section>
+        <section class="clipart-section">
+            <div>
+                <img src="@/assets/join/staff/clipart.png" />
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.row-1 {
+    overflow: auto;
+
+    > section,
+    > div {
+        width: 50%;
+        float: left; // 并排两个 block
+    }
+
+    div {
+        img {
+            height: 100%;
+            width: 100%;
+        }
+    }
+}
+
+.join-section,
+.clipart-section {
+    text-align: center;
+}
+
+.join-section {
+    p,
+    a {
+        margin: 15px auto;
+    }
+
+    p {
+        width: 100%;
+        text-align: center;
+    }
+
+    a {
+        width: 50%;
+        text-align: right;
+    }
+}
+</style>
