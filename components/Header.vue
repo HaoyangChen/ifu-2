0b88ec74cec4edcfcff5dfcd25b3fe1817ecc4c2
Tigojiang <tigojian@usc.edu>
2021-01-26 23:43:39 +0800

    change defaul hamburger

--- components/Header.vue
+++ components/Header.vue
@@ -344,7 +344,7 @@ export default {
             showAboutMenu: false,
             showServiceMenu: false,
             showConnectMenu: false,
-            showNav: false,
+            showNav: true,
         };
     },
     computed: {
